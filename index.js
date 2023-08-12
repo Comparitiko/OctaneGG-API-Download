a817bf66a48b8dbf77d3fecce4c96a9ccb197f2c
Comparitiko <gabigcy@gmail.com>
2023-08-12 16:40:47 +0200

    Added getEvents

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 import { writeFile, mkdir, rm } from 'node:fs/promises'
 import path from 'node:path'
 
-const baseURL = 'https://zsr.octane.gg/'
 const headers = { 'User-Agent': 'OctaneGG-API-Download' }
-const perPage = 200
+const DB_PATH = path.join(process.cwd(), './database/')
+let page = 1
 let pageSize = 200
-let Page = 1
 
-async function createDir () {
-  const DB_PATH = path.join(process.cwd(), './database')
-  await mkdir(DB_PATH)
-  return DB_PATH
+async function createDbPath () {
+  try {
+    await mkdir(DB_PATH)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 async function removeDir () {
-  await rm(DB_PATH, { force: true, recursive: true })
+  try {
+    await rm(DB_PATH, { force: true, recursive: true })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function createDir (dirName) {
+  try {
+    const filePath = `${DB_PATH}${dirName}`
+    await mkdir(filePath)
+    return filePath
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 async function getEvents () {
   let events = []
-  while (pageSize === 200) {
-    await fetch(`${baseURL}events?Page=${Page}&perPage=${perPage}`, headers)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        console.log('Pagina: ', Page)
-        pageSize = data.pageSize
-        events = events.concat(data)
-        Page += 1
-      })
+  const filePath = await createDir('Events/')
+  try {
+    while (pageSize === 200) {
+      await fetch(`https://zsr.octane.gg/events?page=${page}&perPage=200`, headers)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          console.log('Page: ', page)
+          pageSize = data.pageSize
+          events = events.concat(data)
+          page += 1
+        })
+    }
+  } catch (err) {
+    console.log(err)
   }
-  await writeFile(`${DB_PATH}events.json`, JSON.stringify(events, null, 2), 'utf-8')
+  await writeFile(`${filePath}events.json`, JSON.stringify(events, null, 2), 'utf-8')
 }
-
-const DB_PATH = await createDir()
-console.log(DB_PATH)
-// await getEvents()
 await removeDir()
+await createDbPath()
+console.log('This is the DB path: ', DB_PATH)
+await getEvents()
