c5585afe36d66eaa66208537903071312d1758e4
Comparitiko <gabigcy@gmail.com>
2023-08-12 15:18:50 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import { writeFile, mkdir, rmdir } from 'node:fs/promises'
+import path from 'node:path'
+
+const baseURL = 'https://zsr.octane.gg/'
+const headers = { 'User-Agent': 'OctaneGG-API-Download' }
+const perPage = 200
+let pageSize = 200
+let Page = 1
+
+async function createDir () {
+  const DB_PATH = path.join(process.cwd(), './database')
+  await mkdir(DB_PATH)
+  return DB_PATH
+}
+
+async function getEvents () {
+  let events = []
+  while (pageSize === 200) {
+    await fetch(`${baseURL}events?Page=${Page}&perPage=${perPage}`, headers)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        console.log('Pagina: ', Page)
+        pageSize = data.pageSize
+        events = events.concat(data)
+        Page += 1
+      })
+  }
+  await writeFile(`${DB_PATH}events.json`, JSON.stringify(events, null, 2), 'utf-8')
+}
+
+const DB_PATH = await createDir()
+console.log(DB_PATH)
+// await getEvents()
