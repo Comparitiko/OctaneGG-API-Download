f7c0b3d3a46ef66e9facdc33f3e326084dffcc13
Gabriel Collado <gabigcy@gmail.com>
2023-08-12 20:34:11 +0200

    Some fixed issues

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { writeFile, mkdir, rm } from 'node:fs/promises'
+import { appendFile, writeFile, mkdir, rm } from 'node:fs/promises'
 import path from 'node:path'
 
 const headers = { 'User-Agent': 'OctaneGG-API-Download' }
-const DB_PATH = path.join(process.cwd(), './database/')
+const DB_PATH = path.join(process.cwd(), './Database/')
 
 async function createDbPath () {
   try {
@@ -31,21 +31,27 @@ async function createDir (dirName) {
   }
 }
 
+async function sleep (ms) {
+  console.log('Sleeping 3 seconds')
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function getEvents () {
   console.log('📩 Getting events 📩')
   let page = 1
-  let pageSize = 200
+  let pageSize = 500
   let events = []
   const filePath = await createDir('Events/')
   try {
-    while (pageSize === 200) {
-      await fetch(`https://zsr.octane.gg/events?page=${page}&perPage=200`, headers)
+    while (pageSize === 500) {
+      await fetch(`https://zsr.octane.gg/events?page=${page}&perPage=500`, headers)
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
           console.log('Page: ', page)
           pageSize = data.pageSize
           events = events.concat(data)
           page += 1
+          await sleep(5000)
         })
     }
   } catch (err) {
@@ -58,66 +64,69 @@ async function getEvents () {
 async function getMatches () {
   console.log('📩 Getting matches 📩')
   let page = 1
-  let pageSize = 200
-  let matches = []
+  let pageSize = 500
   const filePath = await createDir('Matches/')
   try {
-    while (pageSize === 200) {
-      await fetch(`https://zsr.octane.gg/matches?page=${page}&perPage=200`, headers)
+    while (pageSize === 500) {
+      await fetch(`https://zsr.octane.gg/matches?page=${page}&perPage=500`, headers)
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
           console.log('Page: ', page)
           pageSize = data.pageSize
-          matches = matches.concat(data)
+          console.log(pageSize)
+          if (page === 1) await writeFile(`${filePath}matches.json`, JSON.stringify(data, null, 2), 'utf-8')
+          await appendFile(`${filePath}matches.json`, JSON.stringify(data, null, 2), 'utf-8')
           page += 1
+          await sleep(5000)
         })
     }
   } catch (err) {
     console.log(err)
   }
-  await writeFile(`${filePath}matches.json`, JSON.stringify(matches, null, 2), 'utf-8')
   console.log('✔️ Finished get matches ✔️')
 }
 
 async function getGames () {
   console.log('📩 Getting games 📩')
   let page = 1
-  let pageSize = 200
-  let games = []
+  let pageSize = 500
   const filePath = await createDir('Games/')
   try {
-    while (pageSize === 200) {
-      await fetch(`https://zsr.octane.gg/games?page=${page}&perPage=200`, headers)
+    while (pageSize === 500) {
+      await fetch(`https://zsr.octane.gg/games?page=${page}&perPage=500`, headers)
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
           console.log('Page: ', page)
           pageSize = data.pageSize
-          games = games.concat(data)
+          console.log(pageSize)
+          if (page === 1) await writeFile(`${filePath}games-${page}.json`, JSON.stringify(data, null, 2), 'utf-8')
+          await appendFile(`${filePath}games-${page}.json`, JSON.stringify(data, null, 2), 'utf-8')
+          await sleep(5000)
           page += 1
         })
     }
   } catch (err) {
     console.log(err)
   }
-  await writeFile(`${filePath}games.json`, JSON.stringify(games, null, 2), 'utf-8')
   console.log('✔️ Finished get games ✔️')
 }
 
 async function getPlayers () {
   console.log('📩 Getting players 📩')
   let page = 1
-  let pageSize = 200
+  let pageSize = 500
   let players = []
   const filePath = await createDir('Players/')
   try {
-    while (pageSize === 200) {
-      await fetch(`https://zsr.octane.gg/players?page=${page}&perPage=200`, headers)
+    while (pageSize === 500) {
+      await fetch(`https://zsr.octane.gg/players?page=${page}&perPage=500`, headers)
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
           console.log('Page: ', page)
           pageSize = data.pageSize
           players = players.concat(data)
           page += 1
+          await sleep(5000)
         })
     }
   } catch (err) {
@@ -129,7 +138,7 @@ async function getPlayers () {
 
 await removeDir()
 await createDbPath()
-await getEvents()
-await getMatches()
+// await getEvents()
+// await getMatches()
 await getGames()
-await getPlayers()
+// await getPlayers()
